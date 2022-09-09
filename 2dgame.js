4abed7153cd8a2927a15e97984ac3b410f4a039a
CodingBDX <antonycharier@gmail.com>
2022-09-09 14:27:24 +0000

    all game freecodecamp add explosion+target+gameover

--- 2dgame.js
+++ 2dgame.js
@@ -191,7 +191,7 @@ console.log(this.keys)
             this.fps = 20;
             this.frameTimer = 0;
             this.frameInterval = 1000 / this.fps;
-            this.speed = 12;
+            this.speed = Math.random() * 12 +5;
             this.markedForDeletion = false;
         }
         draw(context) {
